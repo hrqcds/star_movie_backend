e6b39b56e98606be6c5f04e952f179fdb48382ab
Henrique <hrqcds.dev@gmail.com>
2023-06-17 23:04:02 -0300

    fix: migrate

--- prisma/migrations/20230618020112_init/migration.sql
+++ prisma/migrations/20230618020112_init/migration.sql
@@ -1,10 +1,13 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
+
 -- CreateTable
 CREATE TABLE "movies" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
-    "note" INTEGER,
+    "note" DOUBLE PRECISION DEFAULT 0,
     "img_url" TEXT NOT NULL,
-    "categoriesId" TEXT,
+    "description" TEXT NOT NULL,
     "userId" TEXT,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3),
@@ -14,20 +17,13 @@ CREATE TABLE "movies" (
 );
 
 -- CreateTable
-CREATE TABLE "categories" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "postsId" TEXT,
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3),
     "deleted_at" TIMESTAMP(3),
@@ -40,7 +36,8 @@ CREATE TABLE "comments" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "note" INTEGER,
+    "note" DOUBLE PRECISION DEFAULT 0,
+    "user_name" TEXT NOT NULL,
     "moviesId" TEXT,
     "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
@@ -48,16 +45,13 @@ CREATE TABLE "comments" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "movies_title_key" ON "movies"("title");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
-ALTER TABLE "movies" ADD CONSTRAINT "movies_categoriesId_fkey" FOREIGN KEY ("categoriesId") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "movies" ADD CONSTRAINT "movies_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "users" ADD CONSTRAINT "users_postsId_fkey" FOREIGN KEY ("postsId") REFERENCES "comments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "comments" ADD CONSTRAINT "comments_moviesId_fkey" FOREIGN KEY ("moviesId") REFERENCES "movies"("id") ON DELETE SET NULL ON UPDATE CASCADE;
