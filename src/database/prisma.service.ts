c763e04821be420e3d32f39e88ed6651c9c2c66e
Henrique <hrqcds.dev@gmail.com>
2023-06-20 18:12:26 -0400

    deploy

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -21,19 +21,19 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
         },
       });
 
-      const movies = [];
-      for (let i = 0; i < 10; i++) {
-        movies.push({
-          title: faker.lorem.words(3),
-          note: faker.number.float({ min: 0, max: 5 }),
-          img_url: faker.image.url(),
-          description: faker.lorem.words(10),
-          userId: user.id,
-        });
-      }
-      await this.movies.createMany({
-        data: movies,
-      });
+      // const movies = [];
+      // for (let i = 0; i < 10; i++) {
+      //   movies.push({
+      //     title: faker.lorem.words(3),
+      //     note: faker.number.float({ min: 0, max: 5 }),
+      //     img_url: faker.image.url(),
+      //     description: faker.lorem.words(10),
+      //     userId: user.id,
+      //   });
+      // }
+      // await this.movies.createMany({
+      //   data: movies,
+      // });
     }
   }
 
